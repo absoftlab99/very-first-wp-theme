18d2d9272735872d88a869e8d5803788ee4213ba
absoftlab99 <abdullahtech2@outlook.com>
2024-12-19 23:23:49 +0600

    biler plate for wp theme development is ready now

--- functions.php
+++ functions.php
@@ -5,3 +5,19 @@ My Theme Function
 
 // Theme Title
 add_theme_support('title-tag');
+
+//Theme CSS and jQuery File calling
+function absl_css_js_calling(){
+    wp_enqueue_style('absl-style', get_stylesheet_uri());
+    wp_register_style('absl-bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.3.3', 'all');
+    wp_enqueue_style('absl-bootstrap');
+    wp_register_style('absl-custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('absl-custom');
+
+    //jQuery
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('absl-bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.3.3', 'true');
+    wp_enqueue_script('absl-main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true');
+}
+add_action('wp_enqueue_scripts', 'absl_css_js_calling');
+
--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
-    
+<body <?php body_class(); ?>>
+
 
 <?php wp_footer(); ?>
 </body>
